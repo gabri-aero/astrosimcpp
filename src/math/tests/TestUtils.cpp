b8b5f377b57ddaa639ed217b17cfab100f298547
Gabriel Valles <gvallesv@outlook.es>
2023-11-26 00:08:20 +0100

    WrapTo2Pi tests

--- src/math/tests/TestUtils.cpp
+++ src/math/tests/TestUtils.cpp
@@ -14,3 +14,9 @@ TEST(TestUtils, DegRad) {
     ASSERT_EQ(deg2rad(15), 15*M_PI/180);
     ASSERT_EQ(rad2deg(1), 180/M_PI);
 }
+
+TEST(TestUtils, WrapTo2Pi) {
+    ASSERT_EQ(wrapTo2Pi(10), 10 - (2*M_PI));
+    ASSERT_EQ(wrapTo2Pi(-1), -1 + (2*M_PI));
+    ASSERT_EQ(wrapTo2Pi(5), 5);
+}
