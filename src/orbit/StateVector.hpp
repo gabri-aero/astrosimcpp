c351c83a512ce6df2494c3112df58715372ef3fe
Gabriel Valles <gvallesv@outlook.es>
2023-11-25 18:19:50 +0100

    Epoch added to StateVector and Orbit

--- src/orbit/StateVector.hpp
+++ src/orbit/StateVector.hpp
@@ -1,5 +1,6 @@
 #include <math/Vector.hpp>
 #include <bodies/Body.hpp>
+#include <time/Epoch.hpp>
 
 #ifndef _STATE_VECTOR_HPP_
 #define _STATE_VECTOR_HPP_
@@ -7,11 +8,13 @@
 class Orbit; // forward class
 
 class StateVector {
+    Epoch epoch;
     math::vector rv;
 public:
-    StateVector(double rx, double ry, double rz, double vx, double vy, double vz);
-    StateVector(math::vector rv);
+    StateVector(double rx, double ry, double rz, double vx, double vy, double vz, Epoch epoch=Epoch());
+    StateVector(math::vector rv, Epoch epoch);
     math::vector get_rv() const;
+    Epoch get_epoch() const;
     Orbit to_orbit(Body central);
     friend std::ostream& operator<<(std::ostream& os, const StateVector& obj);
 };
