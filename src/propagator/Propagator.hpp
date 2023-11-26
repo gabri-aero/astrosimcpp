30b2282459fffdf1842a3808674230526a4697b0
Gabriel Valles <gvallesv@outlook.es>
2023-11-26 19:32:27 +0100

    Comments and minor changes to propagator

--- src/propagator/Propagator.hpp
+++ src/propagator/Propagator.hpp
@@ -19,19 +19,53 @@ private:
 
     math::vector get_X();
 public:
+    /**
+     * @brief Default propagator constructor
+    */
     Propagator() = default;
+
+    /**
+     * @brief Propagator constructor
+     * @param start start Epoch
+     * @param end end Epoch
+    */
     Propagator(Epoch start, Epoch end);
 
+    /**
+     * @brief Propagator start epoch setter
+    */
     void set_start(Epoch start);
+    /**
+     * @brief Propagator end epoch setter
+    */
     void set_end(Epoch end);
 
+    /**
+     * @brief Add bodies to the propagator
+     * @param args an arbitrary of bodies can be passed as an argument.
+    */
     template<typename... Args> // I would like to define this in my .cpp file but it seems not to be possilbe
     void add_bodies(Args&... args) {
         (bodies.push_back(&args), ...);
     }
+    /**
+     * @brief Add list of bodies to the propagator (mainly implemented for Python wrapper purposes)
+     * @param body_list vector of bodies to be included for propagation
+    */
+    void add_body_list(const std::vector<Body>& body_list) {
+        for (const auto& body : body_list) {
+            bodies.push_back(body);
+        }
+    }
 
+    /**
+     * @brief Integrator setter
+    */
     void set_integrator(std::shared_ptr<BaseIntegrator> integrator);
 
+    /**
+     * @brief Run propagation
+    */
     void run();
 };
 
