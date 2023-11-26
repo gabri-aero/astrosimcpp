1e88d9c38d2c2bf9fd3f8c7e110c45794df63d4c
Gabriel Valles <gvallesv@outlook.es>
2023-11-26 15:57:30 +0100

    Numerical module comments

--- src/numerical/Euler.hpp
+++ src/numerical/Euler.hpp
@@ -3,10 +3,18 @@
 #ifndef _EULER_HPP_
 #define _EULER_HPP_
 
+/**
+ * @class Euler
+ * @brief Integrator that makes use of the Euler method
+*/
 class Euler: public BaseIntegrator {
 public:
     using BaseIntegrator::BaseIntegrator;
 
+    /**
+     * @brief Euler method integration algorithm
+     * @return next iteration result
+    */
     std::pair<double, math::vector> integrate() override;
 };
 
--- src/numerical/RK4.hpp
+++ src/numerical/RK4.hpp
@@ -7,6 +7,10 @@ class RK4: public BaseIntegrator {
 public:
     using BaseIntegrator::BaseIntegrator;
 
+    /**
+     * @brief RK4 method integration algorithm
+     * @return next iteration result
+    */
     std::pair<double, math::vector> integrate() override;
 };
 
