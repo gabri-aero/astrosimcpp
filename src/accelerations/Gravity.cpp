538b6eb1e9b6225b62c9bced9f7d4ba3456773fb
Gabriel Valles <gvallesv@outlook.es>
2023-11-26 12:45:21 +0100

    Gravity module comments

--- src/accelerations/Gravity.cpp
+++ src/accelerations/Gravity.cpp
@@ -2,8 +2,11 @@
 #include <bodies/Body.hpp>
 
 math::vector gravity(Body i, Body j) {
+    // Compute relative position vector
     math::vector rij = j.get_pos() - i.get_pos();
+    // Retrieve body mass
     double mi = i.get_mass();   
     double mj = j.get_mass();
+    // Point-mass gravity
     return G*mj/pow(norm(rij), 3)*rij;
 }
