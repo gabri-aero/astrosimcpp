61c0251de72d2bf7bd72bcd4e6f278b62f4566c6
Gabriel Valles <gvallesv@outlook.es>
2023-11-26 13:19:45 +0100

    Frame module comments

--- src/frames/FrameUtils.hpp
+++ src/frames/FrameUtils.hpp
@@ -3,6 +3,11 @@
 #ifndef _FRAME_UTILS_HPP_
 #define _FRAME_UTILS_HPP_
 
+/**
+ * Compute Earth Rotation Angle according to Vallado (2013)
+ * 
+ * @param epoch Epoch object
+*/
 double era(const Epoch& epoch);  
 
 #endif //_FRAME_UTILS_HPP_
