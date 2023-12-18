3d58c49c5086ac2ba54952bc166de1746cee935f
Gabriel Valles <gvallesv@outlook.es>
2023-12-18 11:42:05 +0100

    Interpolation added to TimeSeries

--- src/time/TimeSeries.hpp
+++ src/time/TimeSeries.hpp
@@ -7,8 +7,31 @@
 template <typename T>
 class TimeSeries : public std::vector<std::pair<Epoch, T>> {
 public:
+    /**
+     * @brief Function that pushes back Epoch, T pair element
+    */
     void add(const Epoch& epoch, const T& value) {
         this->push_back(std::make_pair(epoch, value));
+    }
+    /**
+     * @brief Retrieves T object given an input Epoch. It applies interpolation.
+     * @param epoch
+    */
+    T get(Epoch epoch) { // TO DO: check that Epoch lies within TimeSeries range
+        int i = 0;
+        while(this->at(i).first < epoch) {
+            i++;
+        }
+        i--; // move one step back
+        if(this->at(i).first == epoch) {
+            return this->at(i).second;
+        } else {
+            double step = this->at(i+1).first - this->at(i).first;
+            double dt = epoch - this->at(i).first;
+            T dvalue = this->at(i+1).second - this->at(i).second;
+            T obj = this->at(i).second + dvalue * dt/step;
+            return obj;
+        }
     }
 };
 
--- src/time/tests/TestTimeSeries.cpp
+++ src/time/tests/TestTimeSeries.cpp
@@ -12,3 +12,13 @@ TEST(TimeSeriesTest, Test) {
     ASSERT_EQ(time_series.at(0).first, epoch);
     ASSERT_EQ(time_series.at(0).second, value);
 }
+
+TEST(TimeSeriesTest, Interpolation) {
+    Epoch epoch{0, TAI, J2000};
+    TimeSeries<double> time_series;
+    double value = 13.4;
+    double value2 = 15.7;
+    time_series.add(epoch, value);
+    time_series.add(epoch.add_days(1), value2);
+    ASSERT_EQ(time_series.get(epoch.add_days(0.3)), 14.09);
+}
