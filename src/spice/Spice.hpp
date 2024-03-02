adbe84477717ee6abfbaa78dd42c38b7a3d32dcd
Gabriel Valles <gvallesv@outlook.es>
2024-03-02 20:52:51 +0100

    Spice support for radii and GM

--- src/spice/Spice.hpp
+++ src/spice/Spice.hpp
@@ -1,1 +1,33 @@
-#include <cspice>
+#include <SpiceUsr.h>
+#include <bodies/Body.hpp>
+#include <math/Matrix.hpp>
+
+namespace spice {
+
+void load_kernel(std::string rel_path) {
+    std::string path = std::string{SPICE_KERNELS_DIR} + '/' + rel_path;
+    furnsh_c( + path.c_str());
+}
+
+void load_default() {
+    load_kernel("de440.bsp");
+    load_kernel("pck00011.tpc");
+    load_kernel("gm_de440.tpc");
+}
+
+std::array<double, 3> get_radii(std::string body) {
+    SpiceInt n;
+    SpiceDouble spice_radii[3];
+    bodvrd_c(body.c_str(), "RADII", 3, &n, spice_radii);
+    std::array<double, 3> radii = {spice_radii[0]*1e3, spice_radii[1]*1e3, spice_radii[2]*1e3};
+    return radii;
+}
+
+double get_mu(std::string body) {
+    SpiceInt n;
+    SpiceDouble spice_gm;
+    bodvrd_c(body.c_str(), "GM", 1, &n, &spice_gm);
+    return spice_gm * 1e9;
+}
+
+}
--- src/spice/tests/TestSpice.cpp
+++ src/spice/tests/TestSpice.cpp
@@ -1,6 +1,11 @@
 #include <gtest/gtest.h>
-#include <SpiceUsr.h>
+#include <stdio.h>
+#include <spice/Spice.hpp>
+#include <filesystem>
 
 TEST(Spice, Test) {
-    furnsh_c("furnsh_ex5.tm");
+    spice::load_default();
+    auto earth_radii = spice::get_radii("NAIAD");
+    std::cout << earth_radii[0] << std::endl;
+    std::cout << spice::get_mu("NAIAD") << std::endl;
 }
