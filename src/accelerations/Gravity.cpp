17a67af8df9e39ae611b8f735737b757511cd3b9
Gabriel Valles <gvallesv@outlook.es>
2024-03-23 12:15:44 +0100

    Point gravity using mu

--- src/accelerations/Gravity.cpp
+++ src/accelerations/Gravity.cpp
@@ -4,9 +4,8 @@
 math::vector PointMass::gravity(const Body& i, const Body& j) {
     // Compute relative position vector
     math::vector rij = j.get_pos() - i.get_pos();
-    // Retrieve body mass
-    double mi = i.get_mass();   
-    double mj = j.get_mass();
+    // Retrieve body j gravitational constant
+    double mu_j = j.get_mu();
     // Point-mass gravity
-    return G*mj/pow(norm(rij), 3)*rij;
+    return mu_j/pow(norm(rij), 3)*rij;
 }
